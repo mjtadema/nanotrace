1ec223a18a4705cb7bcb20c3ef60b053474391f9
Matthijs Tadema <M.J.Tadema@protonmail.com>
2025-02-12 15:25:49 +0100

    Add multiprocessing using joblib

--- pipeline.py
+++ pipeline.py
@@ -1,11 +1,15 @@
 from pathlib import Path
+import multiprocessing as mp
 
+import numpy as np
+import pandas as pd
 from anytree import PreOrderIter, LevelOrderGroupIter, NodeMixin
+from joblib import Parallel, delayed
 from pyabf import ABF
+from tqdm.auto import tqdm
 
 from .utils import PoolMixin
 from .segment import Segment
-
 
 ################
 ### PIPELINE ###
@@ -15,27 +19,26 @@ class AbfRoot(NodeMixin, PoolMixin):
     """
     Special segment that acts as the head of the pipeline
     """
-    def __init__(self, pipeline, abf=None):
+    def __init__(self, pipeline, extractors=None, columns=None, abf=None, keep_steps=True):
         self.abf = abf
         self.t = []
         self.y = []
         self.sweeps = []
         self.name = 'abf'
-        self._features = []
+        self._features = None
+        self.extractors = extractors
+        self.columns = columns
+        self.keep_steps = keep_steps
         for i in range(self.abf.sweepCount):
             self.abf.setSweep(i)
-            self.sweeps.append(Segment(self.abf.sweepX, self.abf.sweepY, pipeline, name='sweep', parent=self))
-        # Generate the tree
-        # TODO remove this but make sure the tree gets generated when we get features etc
-        for node in PreOrderIter(self):
-            pass
+            self.sweeps.append(Segment(self.abf.sweepX, self.abf.sweepY, [], pipeline, name='sweep', parent=self, keep_steps=True))
 
     def __str__(self):
         return "ABF from %s" % (self.abf.abfFilePath)
 
     @property
     def events(self):
-        return self.leaves
+        return self.levels[-1]
 
     @property
     def features(self):
@@ -43,30 +46,51 @@ class AbfRoot(NodeMixin, PoolMixin):
         Pipeline root only pools features from sweeps
         """
         # Cache features
+        if self.extractors is None: return
         if self._features is None:
-            self._features = self.pool()
+            # Optimization: calculate features for events in parallel ahead of time
+            features = []
+            cols = []
+            for extractor in self.extractors:
+                extracted = Parallel(n_jobs=4, backend='multiprocessing')(
+                    delayed(extractor)(event.t,event.y)
+                    for event in tqdm(self.events, desc="extracting features %s"%extractor.__name__)
+                )
+                extracted = np.array(extracted)
+                if len(extracted.shape) == 1:
+                    extracted = extracted[...,None]
+                features.append(extracted)
+                cols.extend([extractor.__name__+'_%d'%i for i in range(extracted.shape[-1])])
+            if not self.columns is None:
+                cols = self.columns
+            self._features = pd.DataFrame(np.hstack(features), columns=cols)
         return self._features
 
     @property
-    def stages(self):
+    def levels(self):
         """
         returns a list of nodes grouped by level
         conceptually as "stages" of the pipeline"
         """
+        if not self.children:
+            # Generate the tree
+            for node in PreOrderIter(self):
+                pass
         return list(LevelOrderGroupIter(self))
 
 class Pipeline:
-    def __init__(self, *pipeline):
+    """Pipeline factory with caching"""
+    def __init__(self, *pipeline, **kwargs):
         self.name = 'root'
-        self._features = None
         self.pipeline = pipeline
         self._cache = {}
+        self.kwargs = kwargs
 
     def __call__(self, abfpath: Path):
         abfpath = Path(abfpath).absolute()
         # Could even use the file hash perhaps...
         if not abfpath in self._cache:
             abf = ABF(abfpath)
-            self._cache[abfpath] = AbfRoot(self.pipeline, abf=abf)
+            self._cache[abfpath] = AbfRoot(self.pipeline, abf=abf, **self.kwargs)
         return self._cache[abfpath]
 
--- segment.py
+++ segment.py
@@ -1,38 +1,44 @@
+import random
+
 from anytree import NodeMixin, RenderTree
 import pandas as pd
-
+from joblib import Parallel, delayed
 from .utils import PoolMixin
+from tqdm.auto import tqdm
+import gc
 
 
 class Segment(NodeMixin, PoolMixin):
-    def __init__(self, t, y, pipeline, name=None, parent=None, label=None):
+    def __init__(self, t, y, l, pipeline, name=None, parent=None, keep_steps=True):
         self.t = t
         self.y = y
+        self.l = l # Extra label given by refiner
         self.parent = parent
-        self.abf = self.parent.abf
         self.name = name
         self.refiner = None
+        self.keep_steps = keep_steps
         self.extractors = []
         self.condensors = []
         self.residual = []
-        self.label = label  # Extra label given by refiner
+        self._features = None
 
         # Consume part of the pipeline
         pipeline = list(pipeline)
         if len(pipeline) > 0:
             for i, func in enumerate(pipeline):
-                # Take extractors until a refiner is encountered
-                if func.tag == 'extractor':
-                    assert len(self.condensors) == 0
-                    self.extractors.append(func)
-                elif func.tag == 'condensor':
-                    assert len(self.extractors) == 0
-                    self.condensors.append(func)
-                elif func.tag == 'refiner':
+                # if func is actually a tuple, treat as extractors
+                if isinstance(func, tuple):
+                    self.extractors.extend(list(func))
+                # Callables are treated as refiners
+                elif hasattr(func, '__call__'):
                     self.refiner = func
+                    # Leave the rest of the pipeline as residual
+                    self.residual = pipeline[i + 1:]
                     break
-            # Leave the rest of the pipeline as residual
-            self.residual = pipeline[i + 1:]
+
+    @property
+    def abf(self):
+        return self.parent.abf
 
     def __repr__(self):
         """Fancy tree rendering"""
@@ -69,22 +75,23 @@ class Segment(NodeMixin, PoolMixin):
     def refine(self):
         """Wrap self.refine to generate new segments"""
         if self.refiner is not None:
-            for t, y, *l in self.refiner(self.t, self.y):
-                l = l[0] if len(l) > 0 else None
-                Segment(t, y, pipeline=self.residual, name=self.refiner.__name__, parent=self, label=l)
+            # Optimization: Generate new segments in parallel
+            # Works best with generating many small segments
+            for seg in Parallel(n_jobs=8)(delayed(Segment)(
+                    t,y,l,pipeline=self.residual, name=self.refiner.__name__
+            ) for t,y,*l in self.refiner(self.t, self.y)):
+                seg.parent = self
+            if not self.keep_steps:
+                self.t = []
+                self.y = []
+                gc.collect()
 
-    def extract(self):
-        """
-        Extract new features from self
-        """
-        # If there are any additional features specified already add them to the df
-        cols = []
-        features = []
-        for extract in self.extractors:
-            cols.append(extract.__name__)
-            features.append(extract(self.t, self.y))
-        features = pd.DataFrame([features], columns=cols)
-        return features
+    # def extract(self):
+    #     """
+    #     Extract new features from self
+    #     """
+    #     # If there are any additional features specified already add them to the df
+    #     return #run_extractors(self.t, self.y, *self.extractors)
 
     # def pool(self):
     #     """
@@ -95,33 +102,40 @@ class Segment(NodeMixin, PoolMixin):
     #     else:
     #         return pd.DataFrame()
 
-    def condense(self):
-        """
-        If there are no condensors, simply pool features from children
-        """
-        pooled = self.pool()
-        if len(self.condensors) > 0:
-            cols = []
-            features = []
-            for condense in self.condensors:
-                cols.append(condense.__name__)
-                features.append(condense(pooled))
-            features = pd.DataFrame([features], columns=cols)
-            return features
-        else:
-            return pooled
+    # def condense(self):
+    #     """
+    #     If there are no condensors, simply pool features from children
+    #     """
+    #     pooled = self.pool()
+    #     if len(self.condensors) > 0:
+    #         cols = []
+    #         features = []
+    #         for condense in self.condensors:
+    #             cols.append(condense.__name__)
+    #             features.append(condense(pooled))
+    #         features = pd.DataFrame([features], columns=cols)
+    #         return features
+    #     else:
+    #         return pooled
 
-    @property
-    def features(self):
-        """
-        Wrapper around extractors to extract features.
-        If no extractors are defined, condense features from children
-        """
-        condensed = self.condense()
-        extracted = self.extract()
-        features = pd.concat([extracted, condensed], axis=1)
-        # Add extra label if we have one
-        if self.label is not None:
-            features[self.name] = self.label
-        return features.dropna()
-
+    # @property
+    # def features(self):
+    #     """
+    #     Wrapper around extractors to extract features.
+    #     If no extractors are defined, condense features from children
+    #     """
+    #     if self._features is None:
+    #         raise NotImplementedError("Features are not yet extracted")
+    #         # condensed = self.condense()
+    #         extracted = self.extract()
+    #         features = pd.concat([extracted, condensed], axis=1)
+    #         # Add extra label if we have one
+    #         if len(self.l) > 0:
+    #             for i, label in enumerate(self.l):
+    #                 features[self.name+"_%d"%i] = label
+    #         self._features = features.dropna()
+    #     return self._features
+    #
+    # @features.setter
+    # def features(self, features):
+    #     self._features = features
