1dbffca7ad8d85a666ae39a07dea0e27df38bc8a
Matthijs Tadema <M.J.Tadema@protonmail.com>
2025-03-17 11:27:43 +0100

    Add docstring

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,3 +1,12 @@
+"""
+author: Matthijs Tadema
+
+Pipeline:
+---------
+`pipeline` is a python library for automated nanopore electrophysiology (1d timeseries)
+manipulation and feature extraction. The central class is the `Pipeline` class, imported from the main module:
+`from pipeline import Pipeline`.
+The main module also imports ABF from pyabf for convenience.
+"""
 from .pipeline import Pipeline
 from pyabf import ABF
-from .refiners import *
