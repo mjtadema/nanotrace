a4bf60484e2a1328b79b74978a63a8d2a83f9010
Matthijs Tadema <M.J.Tadema@protonmail.com>
2025-03-17 11:31:29 +0100

    Add docstrings to stages

--- pipeline/stages.py
+++ pipeline/stages.py
@@ -1,8 +1,75 @@
 """
-Functions to be used as refinement steps in a pipeline
-These must take time and current arrays as arguments
-and return (or yield) an iterator of "refined" time and current arrays
+Pipeline stages
+---------------
+Functions to be used as stages in a pipeline
+These must take time and current arrays as positional arguments
+and return an iterator of new time and current arrays
+
+Currently implemented stages:
+-----------------------------
+ `lowpass(cutoff_fq, fs, order=10)`:
+    Apply a lowpass filter with `cutoff_fq` as the cutoff frequency in Hz, `fs`
+    as the sampling rate and `order` as the order of the filter. The sampling rate
+    can be extracted from an abf file using `ABF().SampleRate`
+
+ `as_ires(minsamples=1000)`        :
+    Calculate the _residual current_ (Ires) from the baseline.
+    Automatically detects the baseline based on a binning approach.
+    `minsamples` determines how many samples a bin needs to be considered
+    a proper level and not just a fast current "spike".
+
+ `trim(left=0, right=1)`           :
+    Trim off this many samples from the `left` or the `right` side.
+    If the sampling rate was assigned to a variable named `fs`,
+    you can use this to calculate how many _seconds_ to trim off each side using `nseconds * fs`.
+
+ `switch()`                        :
+    Segment a gapfree trace based on large, short, current spikes cause by manual voltage switching.
+
+ `threshold(lo,hi)`                :
+    Segment an input segment by consecutive stretches of current between `lo` and `hi`.
+
+ `levels(n, tol=0, sortby='mean')` :
+    Detect sublevels by fitting a [gaussian mixture model]
+    (https://scikit-learn.org/stable/modules/generated/sklearn.mixture.GaussianMixture.html).
+    Use `n` to set the number of gaussians to fit, `tol` is a number between 0 and 1
+    and controls how much short spikes are tolerated. `sortby` controls how the gaussians
+    are labeled, can be sorted by "mean" or by "weight" (weight being the height of the gaussian).
+
+Custom stages:
+--------------
+    Each stage is a callable that takes _only_ a time array and a current array as positional arguments.
+    Additional parameters to stages can be passed by using `functools.partial` or by decorating
+    a function with the `partial` decorator included in this library.
+    These are typically defined as generators, yielding zero, one or more "segments" derived from the input.
+    If the stage does not yield zero segments, it acts as a filter(1).
+    A stage can yield a single segment, this is the case with the lowpass filter for example(2).
+    Most stages yield several segments and thus the tree is constructed step by step(3).
+
+    Example 1:
+    --------::
+
+        def stage(time, current):
+            if condition:
+                yield time, current
+
+    Example 1:
+    --------::
+
+        def stage(time, current):
+            new_current = f(current)
+            yield time, current
+
+    Example 3:
+    --------::
+
+        def stage(time, current):
+            for new_time, new_current in f(time, current):
+                yield new_time, new_current
+
 """
+from functools import wraps
+
 import numpy as np
 from scipy import signal
 from scipy.signal import find_peaks
@@ -31,7 +98,15 @@ def switch(t,y):
 
 @partial
 def lowpass(t,y, *, cutoff_fq, fs, order=10):
-    """Wrap a lowpass butterworth filter"""
+    """
+    Wrap a lowpass butterworth filter
+    :param t:
+    :param y:
+    :param cutoff_fq:
+    :param fs:
+    :param order:
+    :return:
+    """
     sos = signal.butter(order, cutoff_fq, 'lowpass', fs=fs, output='sos')
     filt = signal.sosfilt(sos, y)
     assert len(filt) == len(t)
@@ -112,3 +187,18 @@ def levels(t, y, *, n, tol=0, sortby='mean'):
     for s, e, l in zip(bounds[:-1], bounds[1:], padded[bounds + 1]):
         # l becomes a feature with function name as column name
         yield t[s:e], y[s:e], l  # This is the way to smuggle out extra information without having access to the segment yet
+
+def volt(c,v):
+    """
+    Given the control voltage array and a target voltage,
+    cache start and end indices in a closure that slice the sweep at the target voltage.
+
+    :param c: (array) control voltage
+    :param v: (float) target voltage
+    :return: function that slices the sweep
+    """
+    start, end = np.arange(len(c))[np.diff(c == v, append=0) != 0]
+    @wraps(volt)
+    def cached(t,y):
+        yield t[start:end], y[start:end]
+    return cached
