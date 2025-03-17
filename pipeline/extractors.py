db968db7e5c182c56bb953bcf52732964739a8b0
Matthijs Tadema <M.J.Tadema@protonmail.com>
2025-03-17 16:39:22 +0100

    Added a decorator to catch errors in extractors. Unfortunately this breaks multiprocessing for now...

--- pipeline/extractors.py
+++ pipeline/extractors.py
@@ -1,16 +1,3 @@
-"""
-Implement built-in feature extractors.
-Three main classes:
-- Global features
-    - can use as `*global_features`
-- Frequency features
-    - only `freq_by_power(t,y,*,n=8,fs)
-    - :param n: number of frequencies to return
-    - :param fs: signal sampling rate in Hz
-- Sequence features
-    - Use when the signal contains sequence information
-    - combined in `*sequence_features`
-"""
 # Copyright 2025 Matthijs Tadema
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,41 +11,67 @@ Three main classes:
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+Implement built-in feature extractors.
+Three main classes:
+- Global features
+    - can use as `*global_features`
+- Frequency features
+    - only `freq_by_power(t,y,*,n=8,fs)
+    - :param n: number of frequencies to return
+    - :param fs: signal sampling rate in Hz
+- Sequence features
+    - Use when the signal contains sequence information
+    - combined in `*sequence_features`
+"""
+
+import logging
 
 from functools import partial
+from .decorators import catch_errors
 
 import numpy as np
 from scipy.signal import welch, periodogram
 from scipy.stats import gaussian_kde, skew, kurtosis
 from sklearn.mixture import GaussianMixture
 
+logger = logging.getLogger(__name__)
+
 
 # Global features
+@catch_errors()
 def mean(t,y):
     return np.mean(y)
 
+@catch_errors()
 def std(t,y):
     return np.std(y)
 
+@catch_errors()
 def dt(t,y):
     return t[-1]-t[0]
 
+@catch_errors()
 def ldt(t,y):
     return np.log(dt(t,y))
 
+@catch_errors()
 def median(t,y):
     return np.median(y)
 
+@catch_errors()
 def _skew(t,y):
     x = np.linspace(0,1,100)
     k = gaussian_kde(y)
     return skew(k(x))
 
+@catch_errors()
 def kurt(t,y):
     x = np.linspace(0,1,100)
     k = gaussian_kde(y)
     return kurtosis(k(x))
 
+@catch_errors(n=2)
 def clst_means(t,y):
     fit_ = GaussianMixture(n_components=2).fit(y.reshape(-1, 1))
     return np.sort(fit_.means_.flatten())
