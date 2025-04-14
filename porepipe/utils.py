7fbb1b1aa228a4df08654347d8dca9a630afa8ee
Matthijs Tadema <M.J.Tadema@protonmail.com>
2025-04-14 14:01:31 +0200

    Just check a list of types for this...

--- porepipe/utils.py
+++ porepipe/utils.py
@@ -25,10 +25,11 @@ from pyabf import ABF
 from scipy.signal import fftconvolve
 
 ABFLike = Union[ABF, str, Path]
+ABFLikeTypes = [ABF, str, Path]
 
 
 def as_abf(abf: ABFLike) -> ABF:
-    if not type(abf) in ABFLike:
+    if not type(abf) in ABFLikeTypes:
         raise TypeError(('Expected an AbfLike, not type', type(abf)))
     if isinstance(abf, str):
         abf = Path(abf)
