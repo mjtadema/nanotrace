66385a3222ecb1f47727962e22b8c0c2b4c6c64d
Matthijs Tadema <M.J.Tadema@protonmail.com>
2025-07-23 12:05:01 +0200

    Fix levels

--- nanotrace/stages.py
+++ nanotrace/stages.py
@@ -428,7 +428,7 @@ def levels(t: np.ndarray, y: np.ndarray, *, fit: None | GaussianMixture=None, n:
     """
     # fit a guassian mixture
     if fit is None:
-        if n > 1:
+        if n <= 1:
             raise StageError("n needs to be greater than 1")
         try:
             fit = GaussianMixture(n_components=n).fit(y.reshape(-1, 1))
